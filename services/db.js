02c91bd7861b30c476123935d073d67a95086dda
Arturo Marruffo <arturo.marruffo@nextlatam.com>
2025-04-16 10:43:59 -0500

    Added .env auth.

--- services/db.js
+++ services/db.js
@@ -1,8 +1,22 @@
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../peruvian-streamers-dota-league-firebase-adminsdk-fbsvc-3e25725b75.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// const db = admin.firestore();
+// module.exports = db;
+
 const admin = require('firebase-admin');
-const serviceAccount = require('../peruvian-streamers-dota-league-firebase-adminsdk-fbsvc-7abc4c13d9.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
+    clientEmail: serviceAccount.client_email,
+  }),
 });
 
 const db = admin.firestore();
